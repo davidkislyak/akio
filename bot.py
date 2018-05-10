f08fb9d931550b8c1614eb3be93d52524b830384
effyn <effyn@gmx.com>
2018-05-10 13:25:04 -0700

    Refactored all database functions, split the save function into a more logical save function and autosave coroutine, fixed message logging, moved cookie drop functionality to a non-command block so that commands are not blocked by wait_for_reaction, fixed the bot loop to support Ctrl-C KeyboardInterrupt, moved variables around, and added a bunch of comments.

--- bot.py
+++ bot.py
@@ -2,111 +2,119 @@ import asyncio, discord, json, random, os, sys, hashlib
 from discord.utils import get as disget
 from datetime import datetime as dt
 
-# global objects for the bot:discord.Client and db:dict
+# global objects for the bot:discord.Client, db:dict, and fstr:str
 bot = discord.Client()
-with open("db.json") as f: db:dict = json.load(f)
+with open("db.json") as f: db, fstr = json.load(f), f.read().strip("\n")
 
-# database functions
-
+# user data management functions
 def isval(uid:str, key:str):
     return True if db.get(uid) and db[uid].get(key) else False
-
-def isgval(key:str):
-    return True if db.get(key) else False
 
 def setval(uid:str, key:str, val):
     if not db.get(uid): db[uid] = {}
     db[uid][key] = val
 
-def setgval(key:str, val):
-    db[key] = val
-
 def addval(uid:str, key:str, val:int, start=0):
     if not db.get(uid): db[uid] = {}
     db[uid][key] = db[uid][key] + val if db[uid].get(key) else start + val
 
-def addgval(key:str, val:int, start=0):
-    db[key] += val if db.get(key) else start + val
-
 def getval(uid:str, key:str, default):
     return db[uid].get(key, default) if db.get(uid) else default
 
-def getgval(key:str, default):
-    return db.get(key, default)
+# I have decided that global value functions are unnecessary since they are at the root of the database
+# just use the db.get(key, default) and db[key] = val syntax for getting and setting global values
 
-# define a function to automatically save our database in db.json
+# a function that gets added as a bot loop task to automatically save the database to db.json
 async def autosave():
     await bot.wait_until_ready()
     while not bot.is_closed:
-        await asyncio.sleep(getgval("save-interval", 10))
-        with open("db.json") as f: fstr = f.read()
-        dbstr = json.dumps(db, separators=(",", ":"))
-        if fstr == dbstr: continue # if file content == database content, sleep again
-        print("Saving... ", end="")
-        with open("db.json", "w") as f: f.write(dbstr)
-        print("saved.")
+        await asyncio.sleep(db.get("save-interval", 10))
+        await save()
+
+# database save function
+async def save(andexit=False):
+    global db, fstr
+    dbstr:str = json.dumps(db, separators=(",", ":"))
+
+    # if database content differs from last saved file content, save database content to db.json
+    if dbstr != fstr:
+        print("[.] Saving...")
+        with open("db.json", "w") as f:
+            f.write(dbstr)
+            fstr = dbstr
+        print("[-] Saved.")
+
+    if andexit: sys.exit(0)
 
 @bot.event
 async def on_ready():
-    print("READDDYYYY!!!!1!!!")
+    print("[-] Ready.")
 
 @bot.event
 async def on_message(msg):
 
+    # prepping some variables for replying to users, verifying commands, initializing 
+    uid, uname, nname = msg.author.id, msg.author.name, msg.author.display_name
+    pfx = db.get("prefix", ";")
+    pfxlen = len(pfx)
+    time, replytime = dt.now(), db.get("replytime", False)
+
+    # check function to use on cookie reaction events
     def cookiecheck(react:discord.Reaction, user:discord.User):
         return user != bot.user and str(react.emoji).startswith(u"\U0001F36A")
 
+    # searches a string for an ping and parses it
+    # if it doesn't find one, searches members for a matching name
     def getuid(search:str):
         if search[:2] == "<@" and search[len(search)-1] == ">": return search[2:len(search)-1]
         user = disget(msg.server.members, name=search)
         if user: return user.id
         return None
 
-    # store the current time and decide if any replies should show the response time
-    time, timer = dt.now(), getgval("timer", False)
-
-    # reply function, embed parameter is optional
+    # reply function, embed parameter is optional, fade will delete messages after a certain amount of time
     async def reply(content:str, dest:discord.Channel=msg.channel, embed:discord.Embed=None, fade:int=None):
-        if timer: content += " (**" + str(int((dt.now()-time).total_seconds()*1000)) + "** ms)"
+        if replytime: content += " (**" + str(int((dt.now()-time).total_seconds()*1000)) + "** ms)"
         sentmessage = await bot.send_message(dest, content, embed=embed)
         if fade:
             asyncio.sleep(fade)
             await bot.delete_message(sentmessage)
         else: return sentmessage
 
-    # getting some values necessary for command handling and reply formatting
-    uid = msg.author.id
-    uname = msg.author.name
-    pfx = getgval("prefix", ";")
-    pfxlen = len(pfx)
-
-    # logging
-    print(str(time)[11:], end="| ")
+    # logging messages (not currently limiting to specific channels)
+    print(str(time)[11:], end=" | ")
     try: print(uname, end=": ")
     except: print(uid, end=": ")
-    try: print(msg.content)
+    try: print(str(msg.content)) # .encode("utf-8")
     except: print("[unprintable message]")
 
-    # if message sent by bot or message too short for command, ignore
+    # if the message was sent by the bot or the message is too short to be a command, exit this event
     if msg.author == bot.user or len(msg.content) <= pfxlen: return
 
-    if random.random() < getgval("drop-chance", 0.3):
-        drop = await reply("A cookie appeared! React with :cookie: to take it!")
-        await bot.add_reaction(drop, u"\U0001F36A")
-        result = await bot.wait_for_reaction(message=drop, check=cookiecheck)
-        addval(result.user.id, "cookies", 1)
-        await bot.edit_message(drop, f"`{result.user.name} took the cookie.`")
-        await bot.clear_reactions(drop)
-
-    # command processing, ignore if the message is not a command
-    args = [s for s in msg.content[pfxlen:].split() if s != ""]\
+    # process the command and args into a list
+    cmdwithargs = [s for s in msg.content[pfxlen:].split() if s != ""]\
              if msg.content[:pfxlen] == pfx\
              else None
-    if not args: return
-    cmd, args = args[0], args[1:]
+
+    # if the message is not a command
+    if not cmdwithargs:
+        # do non-command logic here!
+        if random.random() < db.get("drop-chance", 0.03):
+            drop = await reply("A cookie appeared! React with :cookie: to take it!")
+            await bot.add_reaction(drop, u"\U0001F36A")
+            result = await bot.wait_for_reaction(message=drop, check=cookiecheck)
+            addval(result.user.id, "cookies", 1)
+            await bot.edit_message(drop, f"`{result.user.display_name} took the cookie.`")
+            await bot.clear_reactions(drop)
+
+        return # since the message is not a command, exit this event
+
+    # now for the command stuff,
+    # break apart the command and the command arguments
+    cmd, args = cmdwithargs[0], cmdwithargs[1:]
+    # represent the list of arguments as a string as well (it's shit code i know)
     argstr = msg.content[pfxlen+len(cmd)+1:] if args else ""
 
-    # commands
+    # finally, COMMANDS
     if cmd == "status" and args:
         setval(uid, "status", argstr)
 
@@ -150,10 +158,10 @@ async def on_message(msg):
         await reply(f"Love meter: **{lovemeter}**%")
 
     elif cmd == "bal":
-        await reply(f"{uname}, you have **{getval(uid, 'gold', 0)}**:large_orange_diamond:")
+        await reply(f"{nname}, you have **{getval(uid, 'gold', 0)}**:large_orange_diamond:")
 
     elif cmd == "cookies":
-        await reply(f"{uname}, you have **{getval(uid, 'cookies', 0)}** :cookie:.")
+        await reply(f"{nname}, you have **{getval(uid, 'cookies', 0)}** :cookie:.")
 
     elif cmd == "viru":
         await reply(random.choice(list(msg.server.members)).name + " is a binch <:grincat:399790921241067521>")
@@ -161,17 +169,15 @@ async def on_message(msg):
     elif cmd == "waifu":
         await bot.send_file(msg.channel, "./waifus/" + random.choice(os.listdir("./waifus")))
 
-    # def check(msg): return msg.startswith(";pick")
-    # usermessage = await bot.wait_for_message(author=msg.author)
-    # if msg.content
-
     # admin commands
-    elif uid in db["admins"]:
+    elif uid in db.get("admins", ["206896737934114819"]):
         if cmd in ("add", "addval", "set", "setval") and len(args) > 1:
             try:
                 db[args[0]] = argstr[len(args[0])+1:]
                 await reply("Value set.")
-            except: await reply(f"Could not create key for arg: {args[0]}")
+            except Exception as e:
+                await reply(f"An error occured when saving value: {args[0]}")
+                print(e)
 
         if cmd in ("data", "global", "globals"):
             gdb = {k: v for k, v in db.items() if (not k.isdigit() or len(k) != 18) and k != "token"}
@@ -189,15 +195,18 @@ async def on_message(msg):
                 else:
                     del db[args[0]]
                     await reply(f"Key {args[1]} deleted.")
-            except KeyError:
+            except KeyError as e:
                 await reply("Key not found.")
+                print(e)
 
-        if cmd in ("rst", "restart"):
-            await reply("Restarting...")
-            os.startfile(getgval("filepath", "C:\\Users\\Admin\\Documents\\Python\\bot\\bot.py"))
-            await bot.logout()
-            sys.exit(0)
+        # if cmd in ("rst", "restart"):
+        #     await reply("Restarting...")
+        #     os.startfile(getgval("filepath", "C:\\Users\\Admin\\Documents\\GitHub\\akio\\bot.py"))
+        #     sys.exit(0)
 
-# add the autosave function to the bot loop
-bot.loop.create_task(autosave())
-bot.run(db["token"])
+try:
+    # add the autosave function to the bot loop
+    bot.loop.create_task(autosave())
+    bot.loop.run_until_complete(bot.start(db["token"]))
+except KeyboardInterrupt:
+    bot.loop.run_until_complete(save(andexit=True))
